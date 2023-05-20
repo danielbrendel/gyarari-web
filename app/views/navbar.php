6fae2bba2a7444da201113b091f2aead3b12af71
Daniel Brendel <dbrendel1988@gmail.com>
2023-05-20 21:41:35 +0200

    Navbar improvements

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -1,5 +1,9 @@
 <nav class="navbar is-black" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
+        <a class="navbar-item navbar-item-brand is-visible-mobile" href="{{ url('/') }}">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo"/>&nbsp;{{ env('APP_NAME') }}
+        </a>
+
         <a role="button" class="navbar-burger burger is-top-5" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
@@ -28,37 +32,37 @@
 
         <div class="navbar-end">
             @if (env('LINK_WEBSITE'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_WEBSITE') }}"><i class="fas fa-globe"></i></a>
             </div>
             @endif
 
             @if (env('LINK_INSTAGRAM'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_INSTAGRAM') }}"><i class="fab fa-instagram"></i></a>
             </div>
             @endif
 
             @if (env('LINK_TWITTER'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_TWITTER') }}"><i class="fab fa-twitter"></i></a>
             </div>
             @endif
 
             @if (env('LINK_DISCORD'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_DISCORD') }}"><i class="fab fa-discord"></i></a>
             </div>
             @endif
 
             @if (env('LINK_TWITCHTV'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_TWITCHTV') }}"><i class="fab fa-twitch"></i></a>
             </div>
             @endif
 
             @if (env('LINK_MASTODON'))
-            <div class="navbar-item">
+            <div class="navbar-item is-inline-block is-navbar-fixed-top">
                 <a href="{{ env('LINK_MASTODON') }}"><i class="fab fa-mastodon"></i></a>
             </div>
             @endif
