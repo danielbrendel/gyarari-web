00d6cf590c320163221e823a361f690243eef476
Daniel Brendel <dbrendel1988@gmail.com>
2023-05-25 10:53:02 +0200

    Fix scroller

--- app/views/scroller.php
+++ app/views/scroller.php
@@ -1,5 +1,5 @@
 <div class="scroll-to-top">
     <div class="scroll-to-top-inner">
-        <a href="javascript:void(0);" onclick="window.scroll({top: 0, left: 0, behavior: 'smooth'});"><i class="fas fa-arrow-up fa-2x up-color"></i></a>
+        <a href="javascript:void(0);" onclick="document.getElementById('app').scroll({top: 0, left: 0, behavior: 'smooth'});"><i class="fas fa-arrow-up fa-2x up-color"></i></a>
     </div>
 </div>
